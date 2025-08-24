88421928a0ee04e27a6cbb644fb44e3bea1b54c5
simbah <wiwarevara@gmail.com>
2025-08-24 15:21:24 +0300

    Update 99nitf.lua

--- 99nitf.lua
+++ 99nitf.lua
@@ -109,7 +109,7 @@ function tptocampfire()
 	end
 
 function fuelit()
-	for i,v in workspace.Items do
+	for i,v in workspace.Items:GetChildren() do
 		if v:GetAttribute("BurnFuel") then
 			store(v)
 			tptocampfire()
